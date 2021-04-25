b12dcf22ea1b10527f19318fbcef01ee04824789
Bernice13 <berniceakpasubi@yahoo.com>
2021-04-25 12:40:53 +0100

    updated modulus code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LabAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int Remainder;
+            Console.WriteLine("A program that returns the remainder of a division operator ");
+            Console.WriteLine("Please enter the first number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //use of the modulus operator
+           Remainder=  num1 % num2;
+
+           Console.WriteLine("The Remainder of first number divided by the second number is {0} ", Remainder);
+
+
+        }
+    }
+}
